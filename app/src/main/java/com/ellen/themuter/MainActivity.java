84edfbdf8fd1ac7f567d32cf501f6903b2addd0f
Ellen <ellenmerchant@gmail.com>
2018-01-28 14:46:41 +0000

    Set ringer volume/mute sound depending on wifi connection.

--- app/src/main/java/com/ellen/themuter/MainActivity.java
+++ app/src/main/java/com/ellen/themuter/MainActivity.java
@@ -1,7 +1,12 @@
 package com.ellen.themuter;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isHome = activeNetwork.getExtraInfo().equals("\"PlusnetWireless405351\"");
+        boolean isWork = activeNetwork.getExtraInfo().equals("\"DevPlus\"");
+
+        if (isWork) {
+            audioManager.setStreamMute(AudioManager.STREAM_RING, true);
+        } else if (isHome) {
+            audioManager.setStreamMute(AudioManager.STREAM_RING, false);
+            audioManager.setStreamVolume(AudioManager.STREAM_RING, 8, 1);
+        } else {
+            audioManager.setStreamMute(AudioManager.STREAM_RING, false);
+        }
     }
 }
